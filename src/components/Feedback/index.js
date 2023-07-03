38bf2a12f213106d670354c1cf77ef097ee0e66d
Rajesh-0223 <rajeswararao0223@gmail.com>
2023-07-03 16:10:31 +0000

    Feedback App

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,68 @@
+// Write your React code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  state = {isFeedbackSelected: false}
+
+  onClickEmoji = () => this.setState({isFeedbackSelected: true})
+
+  renderFeedbackQuestion = () => {
+    const {resources} = this.props
+    const {emojis} = resources
+
+    return (
+      <div className="feedback-question-container">
+        <h1 className="question-heading">
+          How satisfied are you with our customer support performance?
+        </h1>
+        <ul className="emojis-list">
+          {emojis.map(emoji => (
+            <li key={emoji.id}>
+              <button
+                type="button"
+                className="emoji-button"
+                onClick={this.onClickEmoji}
+              >
+                <img src={emoji.imageUrl} className="emoji" alt={emoji.name} />
+                <span className="emoji-name">{emoji.name}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderThankYouScreen = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="thank-you-container">
+        <img src={loveEmojiUrl} className="love-emoji" alt="love emoji" />
+        <h1 className="thank-you-text">Thank You!</h1>
+        <p className="description">
+          We will see your feedback to improve our customer support performance.
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isFeedbackSelected} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="feedback-container">
+          {isFeedbackSelected
+            ? this.renderThankYouScreen()
+            : this.renderFeedbackQuestion()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Feedback
